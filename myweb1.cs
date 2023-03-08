915f10e2d0a65334ecf74e350a7ec2a390d41121
Thristan Torno <torno.thristan@auf.edu.ph>
2023-03-08 15:05:29 +0800

    My website initial files

---
+++ myweb1.cs
@@ -0,0 +1,131 @@
+.1_2 { 
+	overflow:hidden;
+	border:1px solid rgba(0, 0, 0, 1);
+}
+.e1_2 { 
+	background-color:rgba(57.587498277425766, 47.74963617324829, 47.74963617324829, 1);
+	width:768px;
+	height:458px;
+	position:absolute;
+}
+.e1_9 { 
+	background-color:rgba(0, 0, 0, 1);
+	width:776px;
+	height:0px;
+	position:absolute;
+	left:0px;
+	top:41px;
+}
+.1_9 { 
+	border:1px solid rgba(0, 0, 0, 1);
+}
+.1_14 { 
+	border:1px solid rgba(0, 0, 0, 1);
+}
+.e1_14 { 
+	width:36px;
+	height:0px;
+	position:absolute;
+	left:593px;
+	top:30px;
+}
+.e1_15 { 
+	color:rgba(0, 0, 0, 1);
+	width:35px;
+	height:15px;
+	position:absolute;
+	left:594px;
+	top:26px;
+	font-family:Inter;
+	text-align:left;
+	font-size:12px;
+	letter-spacing:0;
+}
+.e1_16 { 
+	color:rgba(0, 0, 0, 1);
+	width:37px;
+	height:15px;
+	position:absolute;
+	left:649px;
+	top:26px;
+	font-family:Inter;
+	text-align:left;
+	font-size:12px;
+	letter-spacing:0;
+}
+.e1_17 { 
+	color:rgba(0, 0, 0, 1);
+	width:48px;
+	height:15px;
+	position:absolute;
+	left:699px;
+	top:26px;
+	font-family:Inter;
+	text-align:left;
+	font-size:12px;
+	letter-spacing:0;
+}
+.e1_18 { 
+	color:rgba(0, 0, 0, 1);
+	width:62px;
+	height:28px;
+	position:absolute;
+	left:11px;
+	top:11px;
+	font-family:Lobster;
+	text-align:left;
+	font-size:22px;
+	letter-spacing:0;
+}
+.e1_20 { 
+	color:rgba(180.8374947309494, 162.00026392936707, 162.00026392936707, 1);
+	width:487px;
+	height:174px;
+	position:absolute;
+	left:236px;
+	top:126px;
+	font-family:Kodchasan;
+	text-align:left;
+	font-size:22px;
+	letter-spacing:0;
+}
+.e9_8 { 
+	width:149px;
+	height:146px;
+	position:absolute;
+	left:30px;
+	top:97px;
+	background-image:url(https://scontent.fmnl9-2.fna.fbcdn.net/v/t1.15752-9/333755765_1612505685877019_6512247522011260375_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGkzoTsD3C8jDYFFvBYu2gSz4MhLq112MrPgyEurXXYymKbBlxMLtn4AVQAuK7bdmhQwKwvxTWeILa1pYPsx8Kr&_nc_ohc=Kd8bq8xQAnYAX_Zz_J8&_nc_ht=scontent.fmnl9-2.fna&oh=03_AdT_Pr_D9oQOB1oPRy8Kj19dGNOAG78qWymRaQDvk_UBuA&oe=6422D4EB);
+	background-repeat:no-repeat;
+	background-size:cover;
+}
+.e9_11 { 
+	width:47px;
+	height:45px;
+	position:absolute;
+	left:511px;
+	top:370px;
+	background-image:url(https://scontent.fmnl9-2.fna.fbcdn.net/v/t1.15752-9/333962968_1148091859209417_5686188413842804758_n.png?stp=cp0_dst-png&_nc_cat=107&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGxqE2BwuPrW68DDKHc0g6VP4u1__Wt_b8_i7X_9a39vxWGNCaE2_jqJblH6ix2LqGmF9gOnlEmQOUcTL2N6549&_nc_ohc=FpG3grtnNnEAX_I2Ngc&_nc_ht=scontent.fmnl9-2.fna&oh=03_AdQXQwvKyjZ7_Io9_pMD4rtbdT0kQdfk9ZReUZP3TE12Ag&oe=6422F71B);
+	background-repeat:no-repeat;
+	background-size:cover;
+}
+.e9_12 { 
+	width:47px;
+	height:45px;
+	position:absolute;
+	left:575px;
+	top:370px;
+	background-image:url(https://scontent.fmnl9-2.fna.fbcdn.net/v/t1.15752-9/333962968_1148091859209417_5686188413842804758_n.png?stp=cp0_dst-png&_nc_cat=107&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGxqE2BwuPrW68DDKHc0g6VP4u1__Wt_b8_i7X_9a39vxWGNCaE2_jqJblH6ix2LqGmF9gOnlEmQOUcTL2N6549&_nc_ohc=FpG3grtnNnEAX_I2Ngc&_nc_ht=scontent.fmnl9-2.fna&oh=03_AdQXQwvKyjZ7_Io9_pMD4rtbdT0kQdfk9ZReUZP3TE12Ag&oe=6422F71B);
+	background-repeat:no-repeat;
+	background-size:cover;
+}
+.e9_13 { 
+	width:47px;
+	height:45px;
+	position:absolute;
+	left:639px;
+	top:370px;
+	background-image:url(https://scontent.fmnl9-2.fna.fbcdn.net/v/t1.15752-9/333962968_1148091859209417_5686188413842804758_n.png?stp=cp0_dst-png&_nc_cat=107&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGxqE2BwuPrW68DDKHc0g6VP4u1__Wt_b8_i7X_9a39vxWGNCaE2_jqJblH6ix2LqGmF9gOnlEmQOUcTL2N6549&_nc_ohc=FpG3grtnNnEAX_I2Ngc&_nc_ht=scontent.fmnl9-2.fna&oh=03_AdQXQwvKyjZ7_Io9_pMD4rtbdT0kQdfk9ZReUZP3TE12Ag&oe=6422F71B);
+	background-repeat:no-repeat;
+	background-size:cover;
+}
